172a5b8edd90967b524a0408a1bddebc24aa2cb3
Nathan Dunn <nathanfdunn@gmail.com>
2020-11-25 21:26:36 -0500

    Committing before we get too far

---
+++ run.py
@@ -0,0 +1,13 @@
+import sys
+# TODO support going through the entire directory
+# This is the path to a given .p8 file
+inputLocation = sys.argv[1]
+
+# Config section
+studio = 'Caterpillar Games'
+itchName = 'caterpillargames'
+outputLocation = "~/Projects/pico8-games"
+
+
+# def compile()
+
