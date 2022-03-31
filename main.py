1fae7ef527b9e0772e2bf594d0882af4b3e5db8e
Nathan Dunn <nathan@fieldstack.com>
2022-03-31 09:47:11 -0400

    Set up tests

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-# This is a sample Python script.
+# from typing import Optional
+#
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name: int):
+#     # Optional[int]:
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#     return None
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == "__main__":
+#     # asdf
+#     x: int = print_hi(3)
+#     print_hi("PyCharm")
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+import unittest
+
+loader = unittest.TestLoader()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == "__main__":
-    # asdf
-    print_hi("PyCharm")
+def get():
+    # raise Exception("oops")
+    start_dir = r"C:\Users\Nathan - Personal\PycharmProjects\p8export\test2"
+    return start_dir
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+suite = loader.discover(get())
+
+
+runner = unittest.TextTestRunner()
+runner.run(suite)
---
+++ test/TestImage.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestImage(unittest.TestCase):
+    def test_works(self) -> None:
+        self.assertTrue(True)
