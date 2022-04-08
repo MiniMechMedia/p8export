e1b2b857f0d412fddc30b91edb663eb20b9afce5
Nathan Dunn <nathan@fieldstack.com>
2022-04-08 14:07:14 -0400

    Just wiring in the folder stuff first

--- src/p8export.py
+++ src/p8export.py
@@ -4,18 +4,24 @@ from ParsedContents import ParsedContents
 from os.path import exists
 from pico8fileparser import Pico8FileParser
 from FileSystemOrchestrator import FileSystemOrchestrator, FileSystemLocations
+from typing import Optional
 
 
 class P8Export:
     # Be warned: will use the directory the p8 file is currently in as the export dir
+
     @classmethod
-    def export(cls, targetFile: Path):
+    def export(cls, targetFile: Path, targetExportDir: Optional[Path] = None):
+        if targetExportDir is not None:
+            raise NotImplemented("this feature is not yet available")
         if not exists(targetFile):
             raise Exception("invalid target")
 
         parsedContents: ParsedContents = Pico8FileParser.parse(targetFile)
         slug: str = parsedContents.metadata.correctedGameSlug
-        targetDir: Path = parsedContents.filePath / ".." / ".." / slug
+        targetDir: Path = (
+            targetExportDir or parsedContents.filePath / ".." / ".." / slug
+        )
 
         locations: FileSystemLocations = FileSystemOrchestrator.prepareExportDir(
             parsedContents.filePath, f"{slug}.p8", targetDir
---
+++ test/TestP8Export.py
@@ -0,0 +1,32 @@
+from src.p8export import P8Export
+from TestFileSystemOperations import TestFileSystemOperations
+from BaseTest import BaseTest, TempFileEnum, TestFileEnum
+import os
+import shutil
+from pathlib import Path
+
+
+class TestP8Export(TestFileSystemOperations):
+    p8exportBaseDir: Path = BaseTest.getTempFolderPath() / "p8export-test"
+
+    # super - soldiers
+    # ORCHESTRATION_TEST_FILE
+    @classmethod
+    def setUpClass(cls) -> None:
+        os.makedirs(cls.p8exportBaseDir, exist_ok=False)
+
+    def setUp(self):
+        self.currentTestFolder: Path = self.p8exportBaseDir / self._testMethodName
+        os.makedirs(self.currentTestFolder, exist_ok=False)
+
+    def test_folder_structure(self):
+        templateDir: Path = self.currentTestFolder / "game-template"
+        os.makedirs(templateDir)
+        p8fileStart: Path = templateDir / "new-game.p8"
+        shutil.copy(
+            self.getTestFilePath(TestFileEnum.BASIC_GAME_TEMPLATE_FILE), p8fileStart
+        )
+        P8Export.export(p8fileStart)
+
+    def test_optional_dir(self):
+        pass
