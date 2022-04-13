cb0eac3b860bf325850ba43fa2d71ada02bbd5f0
Nathan Dunn <nathanfdunn@gmail.com>
2022-04-13 08:13:27 -0400

    TODO add markdown compilation target or something

--- src/ItchDescriptionCompilationTarget.py
+++ src/ItchDescriptionCompilationTarget.py
@@ -5,13 +5,14 @@ from pathlib import Path
 
 
 class ItchDescriptionCompilationTarget(CompilationTarget):
-    @classmethod
-    def compileDescription(cls, data: object) -> str:
-        pass
-
-    @classmethod
-    # I suppose this is where the Itch API would come in...butler and such??
-    def compileDescriptionToFile(cls, data: object, outputFile: Path) -> None:
-        contents = cls.compileDescription(data)
-        with open(outputFile, "w") as file:
-            file.write(contents)
+    pass
+    # @classmethod
+    # def compileDescription(cls, data: object) -> str:
+    #     pass
+    #
+    # @classmethod
+    # # I suppose this is where the Itch API would come in...butler and such??
+    # def compileDescriptionToFile(cls, data: object, outputFile: Path) -> None:
+    #     contents = cls.compileDescription(data)
+    #     with open(outputFile, "w") as file:
+    #         file.write(contents)
