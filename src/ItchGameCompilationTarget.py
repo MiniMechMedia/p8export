6688b4cfe4c473d88257ff35e33ba63d5e803502
Nathan Dunn <nathanfdunn@gmail.com>
2022-04-13 08:46:12 -0400

    Can log into itch

--- src/ItchGameCompilationTarget.py
+++ src/ItchGameCompilationTarget.py
@@ -5,17 +5,33 @@ from pathlib import Path
 
 # import selenium
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
+from decouple import config
 
 
 class ItchGameCompilationTarget(CompilationTarget):
     @classmethod
     def uploadToItch(cls, parsedContents: ParsedContents):
         # selenium.
-        browser: Chrome = Chrome()
-        browser.get("https://itch.io")
+        browser: Chrome
+        with Chrome() as browser:
+            browser.get("https://itch.io/login")
+            username: WebElement = browser.find_element(By.NAME, "username")
+            username.send_keys(config("ITCH_USERNAME"))
 
-    pass
+            password: WebElement = browser.find_element(By.NAME, "password")
+            password.send_keys(config("ITCH_PASSWORD"))
+
+            form: WebElement = browser.find_element(
+                By.CSS_SELECTOR, ".login_form_widget form"
+            )
+            form.submit()
+            import time
+
+            time.sleep(1000)
+
     # @classmethod
     # def compileDescription(cls, data: object) -> str:
     #     pass
