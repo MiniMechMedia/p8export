4c81760a1f8fa2735bf2d5259548d291d01a3a97
Nathan Dunn <nathanfdunn@gmail.com>
2022-06-18 16:30:31 -0400

    Finished tests for comments

--- test/testP8FileTransformation.py
+++ test/testP8FileTransformation.py
@@ -19,11 +19,35 @@ class TestP8FileTransformation(BaseTest):
             expected=TestFileEnum.GAME_CART_TEST_FILE_TRANSFORMED_EXPECTED
         )
 
-    def testGameDescriptionCommentsAreIdempotent(self):
-        pass
-
     def testGameDescriptionCommentsCanBeAdded(self):
-        pass
+        contents: str = self.getTestFilePath(TestFileEnum.GAME_CART_TEST_FILE).read_text()
+        contents = contents.replace('--{gamename}\n--{authorinfo}\n', '')
+        copyLoc: Path = self.getTempFilePath(TempFileEnum.GAME_CART_TEST_FILE_TRANSFORMED_COPY_LOCATION)
+        copyLoc.write_text(
+            data = contents
+        )
+        parsed: ParsedContents = Pico8FileParser.parse(filePath=copyLoc)
+        P8FileTransformerCompilationTarget.transformP8File(p8FilePath=copyLoc, parsed=parsed)
+        self.assertFilesEqual(
+            actual=TempFileEnum.GAME_CART_TEST_FILE_TRANSFORMED_COPY_LOCATION,
+            expected=TestFileEnum.GAME_CART_TEST_FILE_TRANSFORMED_EXPECTED
+        )
+
+    def testGameDescriptionCommentsAreIdempotent(self):
+        contents: str = self.getTestFilePath(TestFileEnum.GAME_CART_TEST_FILE).read_text()
+        copyLoc: Path = self.getTempFilePath(TempFileEnum.GAME_CART_TEST_FILE_TRANSFORMED_COPY_LOCATION)
+        copyLoc.write_text(
+            data=contents
+        )
+        parsed: ParsedContents = Pico8FileParser.parse(filePath=copyLoc)
+        P8FileTransformerCompilationTarget.transformP8File(p8FilePath=copyLoc, parsed=parsed)
+        P8FileTransformerCompilationTarget.transformP8File(p8FilePath=copyLoc, parsed=parsed)
+        self.assertFilesEqual(
+            actual=TempFileEnum.GAME_CART_TEST_FILE_TRANSFORMED_COPY_LOCATION,
+            expected=TestFileEnum.GAME_CART_TEST_FILE_TRANSFORMED_EXPECTED
+        )
+
+
 
 
 
